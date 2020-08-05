80da5446b0d0a969ec1d308016e039a32ffaa7d7
Novicetheaf <edmundocall.eoc@gmail.com>
2020-08-05 15:15:30 +0100

    added protractor demo

---
+++ ConvertedJSFiles/config.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    // directConnect: true,
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    capabilities: {
+        'browserName': 'chrome'
+    },
+    framework: 'jasmine',
+    specs: ['./specs/**/*.js'],
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 90000
+    },
+    onPrepare: function () {
+        var globals = require('protractor');
+        var browser = globals.browser;
+        browser.manage().window().maximize();
+        browser.manage().timeouts().implicitlyWait(5000);
+    }
+};
